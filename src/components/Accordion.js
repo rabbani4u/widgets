88645fe8d522c5ff09931fd42814ccb160cce96f
rabbani4u <raabbani.iu31@gmail.com>
2023-03-12 00:48:19 +0600

    update

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -3,13 +3,13 @@ import React from "react";
 function Accordion({ items }) {
   const renderedItems = items.map(item => {
     return (
-      <div key={item.title}>
+      <React.Fragment key={item.title}>
         <div className="title active">
           <i className="dropdown icon"></i>
           {item.title}
         </div>
         <div className="title active">{item.content}</div>
-      </div>
+      </React.Fragment>
     );
   });
   return <div className="ui styled accordion">{renderedItems}</div>;
